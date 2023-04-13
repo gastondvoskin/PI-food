bfd8e39a50ecf97912ec38bc02f108d003e785dc
gastondvoskin <dvoskingaston@gmail.com>
2023-04-13 18:26:26 -0300

    added route get /recipes?name=pollo

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -9,7 +9,6 @@ const { Recipe } = require('../db.js');     // tono. importo el Modelo. A futuro
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
 const router = Router();
 
 // Configurar los routers
@@ -18,11 +17,14 @@ const router = Router();
 
 // tono inicio
 
+
+//
+
 router.get('/recipes/:idRecipe', async (req, res) => {
     try {
         const { idRecipe } = req.params;
         const source = isNaN(idRecipe) ? "DB" : "apiExterna";       // DB tiene UUID (NaN), apiExterna tiene id numérico
-        console.log('source:' , source);
+        // console.log('source:' , source);
 
         let recipeById;
         if(source === "DB") {
@@ -45,18 +47,61 @@ router.get('/recipes/:idRecipe', async (req, res) => {
 
 // INSERT INTO "Recipes" (id, name, image, summary, healthscore, instructions)
 // VALUES ('d936ed1c-3a3e-43b3-b013-f328e10cfa87', 'Spaghetti Carbonara', 'https://www.example.com/spaghetti-carbonara.jpg', 'A classic Italian dish.', '90', '1. Cook the pasta. 2. Fry.');
+// INSERT INTO "Recipes" (id, name, image, summary, healthscore, instructions)
+// VALUES ('d936ed1c-3a3e-43b3-b013-f328e10cfa80', 'Spaghetti Carbonara', 'https://www.example.com/spaghetti-carbonara.jpg', 'A classic Italian dish.', '90', '1. Cook the pasta. 2. Fry.');
+// INSERT INTO "Recipes" (id, name, image, summary, healthscore, instructions)
+// VALUES ('d936ed1c-3a3e-43b3-b013-f328e10cfa81', 'pollo', 'https://www.example.com/spaghetti-carbonara.jpg', 'A classic Italian dish.', '90', '1. Cook the pasta. 2. Fry.');
+// INSERT INTO "Recipes" (id, name, image, summary, healthscore, instructions)
+// VALUES ('d936ed1c-3a3e-43b3-b013-f328e10cfa82', 'Cannellini Bean and Asparagus Salad with Mushrooms', 'https://www.example.com/spaghetti-carbonara.jpg', 'A classic Italian dish.', '90', '1. Cook the pasta. 2. Fry.');
+
+
+
+   
+router.get('/recipes', async (req, res) => {        // puede tener query: name?=ejemplo 
+    // A futuro implementar que busque con mayúsculas o minúsculas y que la búsqueda no requiera ser exacta. 
+    try {
+        const { name } = req.query;
+
+        // implementación para la DB
+        const dbRecipesByNameRaw = await Recipe.findAll({ 
+            where: { name }
+        });
+        const dbRecipesByNameClean = dbRecipesByNameRaw.map((dbRecipe) => dbRecipe.dataValues);
+        console.log('dbRecipesByNameClean: ', dbRecipesByNameClean);
+
+        // implementación para la API externa
+        let apiRecipesRaw = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true`); 
+        apiRecipesRaw = apiRecipesRaw.data.results;
+
+        const apiRecipesByNameRaw = apiRecipesRaw.filter((apiRecipe) => apiRecipe.title === name);
+        const apiRecipesByNameClean = apiRecipesByNameRaw.map((apiRecipe) => {
+            return {
+                id: apiRecipe.id,
+                title: apiRecipe.title,
+                image: apiRecipe.image,
+                summary: apiRecipe.summary,
+                healthscore: apiRecipe.healthScore,         // ojo la mayúscula y minúscula
+                instructions: apiRecipe.analyzedInstructions,
+                created: false
+            }
+        }); 
+        console.log('apiRecipesByNameClean: ', apiRecipesByNameClean);
+
+        // unificación de DB con API externa
+        const allRecipesByName = [...dbRecipesByNameClean, ...apiRecipesByNameClean];
+        console.log('allRecipesByName: ', allRecipesByName);
+
+        // response
+        res.status(200).send(allRecipesByName); 
+    } catch (error) {
+        res.status(400).send({error: error.message});
+    }
+});  
 
 
 
 
 
-
-
-
-// "https://api.spoonacular.com/recipes/{id}/information"
-
-
-// localhost:3001/recipes/:idRecipe
 
 
 //tono fin
