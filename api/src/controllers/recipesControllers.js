1924b3d917f495fa7f7dba3c0bfca682c9761489
gastondvoskin <dvoskingaston@gmail.com>
2023-04-18 11:39:16 -0300

    implemented searchRecipesByName. added inexact queries

--- api/src/controllers/recipesControllers.js
+++ api/src/controllers/recipesControllers.js
@@ -1,6 +1,7 @@
 const axios = require("axios"); 
 require('dotenv').config(); 
 const { Recipe } = require('../db.js'); 
+const { Op } = require('sequelize');
 
 const { API_KEY } = process.env; 
 
@@ -31,10 +32,8 @@ const searchRecipeById = async (id) => {
 
 
 
-
-// nuevo. inicio.
 const searchAllRecipes = async () => {
-    // A futuro implementar que busque con mayúsculas o minúsculas y que la búsqueda no requiera ser exacta.
+    // A futuro implementar que no retorne sólo 10 recetas. 
     const dbAllRecipesRaw = await Recipe.findAll(); 
     const dbAllRecipesClean = dbAllRecipesRaw.map((dbRecipe) => dbRecipe.dataValues);
     
@@ -56,16 +55,43 @@ const searchAllRecipes = async () => {
     const allRecipes = [...dbAllRecipesClean, ...apiAllRecipesClean];
     return allRecipes;
 };
-// nuevo. fin. 
 
 
 
+// nuevo. inicio.
+const searchRecipesByName = async (name) => {
+    // A futuro implementar que busque con mayúsculas o minúsculas y que la búsqueda no requiera ser exacta.
+    const dbRecipesByNameRaw = await Recipe.findAll({ 
+        where: {
+             name: {
+                [Op.iLike]: `%${name.toLowerCase()}%`           // nuevo
+             }
+        }
+    });
+    const dbRecipesByNameClean = dbRecipesByNameRaw.map((dbRecipe) => dbRecipe.dataValues);
 
-const searchRecipesByName = async () => {
 
+    let apiAllRecipesRaw = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true`); 
+    apiAllRecipesRaw = apiAllRecipesRaw.data.results;
+
+    const apiRecipesByNameRaw = apiAllRecipesRaw.filter((apiRecipe) => apiRecipe.title.toLowerCase().includes(name.toLowerCase()));  // nuevo. ojo name en vez de title
+
+    const apiRecipesByNameClean = apiRecipesByNameRaw.map((apiRecipe) => {
+        return {
+            id: apiRecipe.id,
+            name: apiRecipe.title,                      // ojo name en vez de title
+            image: apiRecipe.image,
+            summary: apiRecipe.summary,
+            healthscore: apiRecipe.healthScore,         // ojo la mayúscula y minúscula
+            instructions: apiRecipe.analyzedInstructions[0].steps.map((step) => step.step),       // [0] porque analyzedInstructions es un array con sólo un elemento
+            created: false
+        }
+    }); 
+
+    const recipesByName = [...dbRecipesByNameClean, ...apiRecipesByNameClean];
+    return recipesByName; 
 };
-
-
+// nuevo. fin. 
 
 
 
